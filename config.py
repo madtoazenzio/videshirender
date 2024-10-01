e7470c0dbae8d3496898beb2234dafdea7836a3a
メ™Asʏɴ Eᴅɪᴛᴢ​ゞメ <89691212+madtoazenzio@users.noreply.github.com>
2024-10-01 11:24:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "codeflix_bots")
 
 #Shortner (token system) 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "droplink.co")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "ba9397cf73004a7d5236f014af404ed712ca1544")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "9ece1a5482fcfd033796280032c2c2746c255ff9")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 600)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/howtoopen88/3") 
